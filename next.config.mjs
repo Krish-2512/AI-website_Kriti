12137498a8a0173f894fbc79c99b7353d1544d16
Krish-2512 <krish12252005@gmail.com>
2026-08-28 18:34:29 +0530

    version_changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    webpackBuildWorker: false,
+  },
   images: {
     unoptimized: true,
     domains: [
